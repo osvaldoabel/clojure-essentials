2cd2785012512bf17b156a0bee2a8791c56d1203
Osvaldo Abel <osvalldo.abel@gmail.com>
2020-03-23 22:19:33 -0300

    References

---
+++ src/tutorial/References.clj
@@ -0,0 +1,22 @@
+(ns tutorial.References)
+
+(defn Refs
+  []
+  (def amount (ref 100))
+  (println @amount)
+
+  (dosync
+    (ref-set amount 110)
+    (println @amount)
+    )
+
+  (dosync
+    (alter amount inc)
+    (println @amount)
+    )
+
+  )
+
+
+
+(Refs)
