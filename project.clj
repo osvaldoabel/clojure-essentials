ac07803a33db1f7e25463d5059641aa12676193e
Osvaldo Abel <osvalldo.abel@gmail.com>
2020-03-23 22:08:41 -0300

    Basic structure

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject tutorial "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [ clojure-lanterna "0.9.4"]]
+  :main ^:skip-aot tutorial.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
